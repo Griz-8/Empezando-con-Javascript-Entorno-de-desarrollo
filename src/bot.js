ccefca413a3f15adff20ead29b39b73e687c33c6
Grizzz88 <mich.griz.ch@gmail.com>
2026-02-26 22:49:26 -0400

    Saludar de acuerdo a la hora

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,20 @@
 function saludar(nombre) {
+  const fecha = new Date();
+  const hora = fecha.getHours();
   let saludo;
-  saludo = 'Hola ' + nombre;
+  if(hora <12)
+  {
+    saludo = 'Buenos dias '
+  }
+   if(hora >=12 && hora < 19)
+  {
+    saludo = 'Buenas tardes '
+  }
+  else
+  {
+    saludo = 'Buenas noches '
+  }
+  saludo = saludo + nombre;
   return saludo;
 }
 
